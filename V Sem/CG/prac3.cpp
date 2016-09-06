16ada49c898664d9bc2911e4aa85766a8bc1ff79
Samarth Sanghi <sanghi.samarth@gmail.com>
2016-09-06 23:50:48 +0530

    Bresenhams Circle & Ellipse Drawing Algo

--- V Sem/CG/prac3.cpp
+++ V Sem/CG/prac3.cpp
@@ -1,95 +1,152 @@
 #include <iostream>
 #include <math.h>
-#include <glut.h>
+#include<Windows.h>
+#include<gl\GL.h>
+#include<gl\GLU.h>
+#include<glut.h>
+
 using namespace std;
 
-int a,b,r,deli,del,deld,c,d;
-
- void setpixel(int a,int b)
- {   glPointSize(5);
-     glColor3f(1.0f, 0.0f, 0.0f);
-     glBegin(GL_POINTS);
-     glVertex2d(a+c,b+d);
-     glVertex2d(-a+c,b+d);
-     glVertex2d(-a+c,-b+d);
-     glVertex2d(a+c,-b+d);
-     glEnd();
-     glFlush();
- }
+int X, Y, i, d, r,a,b;
+int xi, yi, AC;
 
 void mh()
 {
-    a++;
-    deli = deli+(2*a)+1;
+	xi = xi+1;
+	i = i+(2*a*a*xi)+a*a;
 }
- 
-void md()
-{
-    a++;
-    b--;
-    deli = deli+(2*a)-(2*b)+2;
-}
-
 void mv()
 {
-    b--;
-    deli = deli-(2*b)+1;
+	yi = yi-1;
+	i = i-(2*b*b*yi)+b*b;
+}
+void md()
+{
+	xi = xi+1;
+	yi = yi-1;
+	i = i+(2*a*a*xi)-(2*b*b*yi)+b*b;
 }
 
 void circle()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    a = 0;
-    b = r;
-    deli = 2*(1-r);
-    while(b >= 0)
-    { 
-        setpixel(a,b);
-        if(deli < 0)
-        {
-            del = (2*deli)+(2*b)-1; 
-            if(del <= 0)
-                mh();
-            else
-                md();
-        }
-        else if(deli > 0)
-        {
-            deld = (2*deli)-(2*a)-1;
-            if(deld <= 0)
-                md();
-            else
-                mv();
-        }
-        else
-            md();
-    }
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	xi = 0;
+	yi = r;
+	i = 2*(1-r);
+	while (yi >= 0)
+	{
+		glBegin(GL_POINTS);
+		glPointSize(5);
+		glVertex2d(xi + X, yi + Y);
+		glVertex2d(xi + X, -yi + Y);
+		glVertex2d((-xi) + X, yi + Y);
+		glVertex2d((-xi) + X, -yi + Y);
+		glEnd();
+		
+		if(i < 0)
+		{
+			d = (2*i)+(2*yi)-1;
+			if (d <= 0)
+				mh();
+			else
+				md();
+		}
+		else if(i > 0)
+		{
+			d = (2*i)+(2*xi)-1;
+			if (d <= 0)
+				md();
+			else
+				mv();
+		}
+		else
+			md();
+	}
+	glFlush();
+}
+
+void ellipse()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	xi = 0;
+	yi = b;
+	i = (a*a)*(1-(2*b));
+	while (yi >= 0)
+	{
+		glColor3f(1.0f, 0.0f, 0.5f);
+		glBegin(GL_POINTS);
+		glPointSize(5);
+		glVertex2d(xi+X, yi+Y);
+		glVertex2d(xi+X, -yi+Y);
+		glVertex2d((-xi)+X, yi+Y);
+		glVertex2d((-xi)+X, -yi+Y);
+		glEnd();
+
+		if(i < 0)
+		{
+			d = (2*i)+(2*a*a*yi)-a*a;
+			if (d <= 0)
+				mh();
+			else
+				md();
+		}
+		else if (i>0)
+		{
+			d = (2*i)+(2*b*b*xi)-a*a;
+			if (d <= 0)
+				md();
+			else
+				mv();
+		}
+		else
+			md();
+	}
+	glFlush();
 }
 
 void Init()
-{ 
-    glClearColor(1.0,1.0,1.0,0); 
-    glColor3f(1.0,0.0,0.0); 
-    glViewport(0 , 0 , 640 , 480);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0 , 640 , 0 , 480);
+{
+	glClearColor(1.0, 1.0, 1.0, 0);
+	glColor3f(1.0, 0.0, 0.0);
+	glViewport(0, 0, 640, 480);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, 640, 0, 480);
 }
 
 int main(int argc, char **argv)
 {
-    glutInit(&argc,argv); 
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); 
-    cout<<"Enter the center co-ordinates:-\n";
-    cin>>c>>d;
-    cout<<"\nEnter the radius of circle: ";
-    cin>>r;
-    
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(0,0);
-    glutCreateWindow("CIRCLE");
-    Init();
-    glutDisplayFunc(circle); 
-    glutMainLoop(); // Enters the Glut event processing loop
-    return 0;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	int ch;
+	glutInitWindowSize(640, 480);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Bresenhans Algorithm");
+	Init();
+	cout << "1.CIRCLE\n2.ELLIPSE\nEnter your choice: ";
+	cin >> ch;
+	if(ch == 1)
+	{
+		a = b = 1;
+		cout << "\nENTER CENTER:-\n";
+		cin >> X >> Y;
+		cout << "\nENTER RADIUS:";
+		cin >> r;
+		glutDisplayFunc(circle);
+
+	}
+	else if(ch==2)
+	{
+		cout << "\nENTER CENTER:-\n";
+		cin >> X >> Y;
+		cout << "\nENTER TWO AXIS:-\n";
+		cin >> a >> b;
+		glutDisplayFunc(ellipse);
+
+	}
+	
+	glutMainLoop(); // Enters the Glut event processing loop
+	return 0;
 }
