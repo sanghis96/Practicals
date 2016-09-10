863b0d06e9cbec7af5104c68e995ab7cb05adfef
Samarth Sanghi <sanghi.samarth@gmail.com>
2016-09-10 16:18:10 +0530

    Client Server

--- V Sem/CN/Prac6(b)Client.java
+++ V Sem/CN/Prac6(b)Client.java
@@ -10,6 +10,8 @@ public class ClientMain
 	public static void main(String[] args) throws UnknownHostException, IOException 
 	{
 		Socket s = new Socket("localhost",6666);
+		BufferedImage img = ImageIO.read(new File("C:\\Users\\cse\\Pictures\\Screenshots\\Screenshot (7).png"));
+		ImageIO.write(img,"png",s.getOutputStream());
 		DataOutputStream d= new DataOutputStream(s.getOutputStream());
 		d.writeUTF("\nHi server");
 		s.close();
