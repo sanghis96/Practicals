6fa5dda668c3a5318b85f590b506aefc5208ce30
sanghis96 <sanghi.samarth@gmail.com>
2016-09-14 01:45:57 +0530

    CRC

--- V Sem/CN/Prac5.java
+++ V Sem/CN/Prac5.java
@@ -16,45 +16,58 @@ public class demo
     	for(int i=0; i<dataLen; i++)
     		CRC[i] = Integer.parseInt(""+dataBits.charAt(i));
     	for(int i=0; i<polyLen-1; i++)
-    		CRC[dataLen+i] = 0;	
+    		CRC[dataLen+i] = 0;
+    	int[] poly = new int[polyLen];
+    	for(int i=0; i<polyLen; i++)
+    		poly[i] = Integer.parseInt(""+polyBits.charAt(i));
     	System.out.print("Rough CRC code: ");
     	for(int i=0; i<dataLen+polyLen-1; i++)
     		System.out.print(CRC[i]);
-    	
-    	System.out.print("Enetr  the CRC code at receiver side: ");
+    	int[] rem = new int[dataLen+polyLen-1];
+    	rem = divide(CRC,poly,dataLen+polyLen-1,polyLen);
+    	System.out.print("\nCRC code is: ");
+    	for(int i=0; i<dataLen+polyLen-1; i++)
+    	{
+    		CRC[i] ^= rem[i];
+    		System.out.print(CRC[i]);
+    	}
+    	System.out.print("\nEnetr the CRC code at receiver side: ");
     	String atReciever = sc.next();
-    	int[] hamCode = new int[len];
-    	for(int i=0,j=0,k=0; i<len; i++)
-    	    if(i == Math.pow(2, j)-1)
-    	    {    hamCode[i] = 0;   j++;    }
-    	    else
-    	        hamCode[i] = Integer.parseInt(""+dataBits.charAt(k++));
-    	for(int i=0,j=0; i<len; i+=(int)Math.pow(2,j++))
+    	int[] recv = new int[atReciever.length()];
+    	int recvLen = atReciever.length();
+    	for(int i=0; i<recvLen; i++)
+    		recv[i] = Integer.parseInt(""+atReciever.charAt(i));
+    	rem = divide(recv,poly,recvLen,polyLen);
+    	int flag = 0;
+    	for(int i=0; i<recvLen; i++)
+    		if(rem[i] == 1)
+    		{
+    			flag = 1;
+    			break;
+    		}
+    	if(flag ==1)
+    		System.out.println("Error in message");
+    	else
+    		System.out.println("No error in message");
+    }
+    private static int[] divide(int[] div, int[] divisor, int divLen, int divisorLen)
+    {
+    	int[] rem = new int[divLen];
+    	for(int i=0; i<divLen; i++)
+    		rem[i] = div[i];
+    	int curr = 0;
+    	while(true)
     	{
-    	    int count = 0;
-    	    for(int k=i; k<len; k+=i+1)
-    	        for(int l=0; l<i+1&&k<len; l++)
-    	            if(hamCode[k++] == 1)
-    	                count++;
-    	    if(count%2 == 0)
-    	        hamCode[i] = 0;
-    	    else
-    	        hamCode[i] = 1;
+    		for(int i=0; i<divisorLen; i++)
+    			if(rem[curr+i] == divisor[i])
+    				rem[curr+i] = 0;
+    			else
+    				rem[curr+i] = 1;
+    		while(rem[curr] != 1 && curr<divLen-1)
+    			curr++;
+    		if(divLen-curr < divisorLen)
+    			break;
     	}
-    	int errorBit = 0;
-    	for(int i=0; i<len; i++)
-    	    if(hamCode[i] != Integer.parseInt(""+atReciever.charAt(i)))
-    	    {
-    	    	errorBit = i+1;
-    	    	break;
-    	    }
-    	System.out.println("Error is in bit number " + errorBit);
-
-    	}
-    	private static void divide(int[] div, int[] divisor, int divLen, int divisorLen)
-    	{
-    	int[] rem = new int[divisorLen];
-
-    	}
-    }	
+    	return rem;
+    }
 }
