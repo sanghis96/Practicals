0f6d28cd8ebe910dac3d2c2dab19e1ca6c1cf2e1
sanghis96 <sanghi.samarth@gmail.com>
2017-02-16 23:40:42 +0530

    fix bugs of prac1

--- VI Sem/DAA/src/Prac1.java
+++ VI Sem/DAA/src/Prac1.java
@@ -1,8 +1,17 @@
-package quicksort;
-
-public class QuickSort
+class Prac1
 {
-    private static int quickSort(int[] a, int left, int right)
+    private static void QuickSort(int[] list)
+    {	quickSort(list, 0, list.length-1);	}
+    private static void quickSort(int[] list, int first, int last)
+    {
+    	if(first < last)
+		{
+			int pivote = partition(list, first, last);
+			quickSort(list, first, pivote-1);
+			quickSort(list, pivote+1, last);
+		}
+    }
+    private static int partition(int[] a, int left, int right)
     {
         int pivot = left;
         int i=left+1, j=right;
@@ -25,23 +34,13 @@ public class QuickSort
             a[j] = a[pivot] - a[j];
             a[pivot] -= a[j];
         }
-         for(int k=0; k<a.length; k++)
-            System.out.print(a[k] + "  ");
-        System.out.println();
         return j;
-    }
-    private static void Quick(int[] a)
-    {
-        int pivote = quickSort(a, 0, a.length-1);
-        quickSort(a, 0, pivote-1);
-        quickSort(a, pivote+1, a.length-1);
-        Quick(a);
     }
     
     public static void main(String[] args)
     {
         int[] array = {8,3,25,6,10,17,1,2,18,5};
-        Quick(array);
+        QuickSort(array);
         for(int i=0; i<array.length; i++)
             System.out.print(array[i] + "  ");
         System.out.println();
