2dca4fa6342e8e5c8a0793548d1e05cc2ce376f8
sanghis96 <sanghi.samarth@gmail.com>
2017-08-23 02:16:31 +0530

    modified workspace

--- VII Sem/DS/VectorClock/src/VectorClock.java
+++ VII Sem/DS/VectorClock/src/VectorClock.java
@@ -1,17 +1,16 @@
-package prac1;
 
 import java.util.Scanner;
 
-public class LamportClock
+public class VectorClock
 {
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter delay: ");
+		System.out.print("Enter delay: ");
         int delay = sc.nextInt();
         
-        System.out.print("\nEnter no of process: ");
+		System.out.print("\nEnter no of process: ");
         int noOfProcess = sc.nextInt();
         
         int maxEvents = 0;
@@ -39,30 +38,38 @@ public class LamportClock
                 hbr[i][j] = sc.nextInt() - 1;
         }
         
-        int[][] result = new int[noOfProcess][maxEvents];
+        int[][][] result = new int[noOfProcess][maxEvents][noOfProcess];
+        
+        //	Initialization
+        for(int i=0; i<noOfProcess; i++)
+        	for(int j=0; j<noOfEvents[i]; j++)
+        		for(int k=0; k<noOfProcess; k++)
+            		result[i][j][k] = 0;
         
         /* 	Implementing IR1:
          * 	increment each event from previous event by delay.
          */
         for(int i=0; i<noOfProcess; i++)
         {
-            result[i][0] = 0;
-            for(int j=1; j<noOfEvents[i]; j++)
-                result[i][j] = result[i][j-1] + delay;
-        }    
-            
+        	result[i][0][i] = 1;
+        	for(int j=1; j<noOfEvents[i]; j++)
+        		result[i][j][i] = result[i][j-1][i] + delay;
+        }
         
         /* 	Implementing IR2:
-         * 	if sending event time stamp + delay is
+         * 	if sending event time stamp is
          * 	maximum than receiving event time stamp
          * 	then reinitialize with the maximum value.
          */
         for(int i=0; i<noOfHBR; i++)
         {
-            if(result[hbr[i][0]][hbr[i][1]]+delay > result[hbr[i][2]][hbr[i][3]])
-                result[hbr[i][2]][hbr[i][3]] = result[hbr[i][0]][hbr[i][1]]+delay;
+            for(int k=0; k<noOfProcess; k++)
+            	if(result[hbr[i][0]][hbr[i][1]][k] > result[hbr[i][2]][hbr[i][3]][k])
+            		result[hbr[i][2]][hbr[i][3]][k] = result[hbr[i][0]][hbr[i][1]][k];
             for(int j=hbr[i][3]+1; j<maxEvents; j++)
-                result[hbr[i][2]][j] = result[hbr[i][2]][j-1] + delay;
+            	for(int k=0; k<noOfProcess; k++)
+            		if(result[hbr[i][2]][j-1][k] > result[hbr[i][2]][j][k])
+            			result[hbr[i][2]][j][k] = result[hbr[i][2]][j-1][k];
         }
         
         //Showing output:-
@@ -70,9 +77,14 @@ public class LamportClock
         {
             System.out.print("\nP" + (i+1) + ": ");
             for(int j=0; j<noOfEvents[i]; j++)
-                System.out.print(result[i][j]+1 + " ");
-        }   
+            {
+            	System.out.print("[");
+            	for(int k=0; k<noOfProcess; k++)
+            		System.out.print(result[i][j][k] + " ");
+            	System.out.print("]  ");
+            } 	
+        }
         
         sc.close();
-    }
+	}
 }
